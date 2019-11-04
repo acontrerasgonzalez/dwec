69d10142c604d0ccec398ae12c5ab9236afa01da
acontrerasgonzalez <57216473+acontrerasgonzalez@users.noreply.github.com>
2019-11-04 11:55:58 +0100

    t4w4

---
+++ tema4/w4-1.js
@@ -0,0 +1,35 @@
+function setCookie(cname, cvalue, exdays) {//creacion de cookie(nombre,valor,expiracion)
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function getCookie(cname) {//obtener cookie(nombre cookie)
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);//decodifica caracteres especiales
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  function checkCookie() {//combrueba si hay cookie y saluda,sino pide cookie
+    var username = getCookie("username");
+    if (username != "") {
+     alert("Welcome again " + username);
+    } else {
+      username = prompt("Please enter your name:", "");
+      if (username != "" && username != null) {
+        setCookie("username", username, 365);
+      }
+    }
+  }
+  checkCookie();
